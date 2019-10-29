ad133921191de5f91f60949a399c9e7d264c3e5f
Sean Wilson <swilso17@uoregon.edu>
2019-10-28 21:37:33 -0700

    Second commit

--- Project2/part1.c
+++ Project2/part1.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <unistd.h>
 
+
+
 int main(int argc, char *argv[]) {
 
   FILE *input;
@@ -13,13 +15,19 @@ int main(int argc, char *argv[]) {
 	size_t inputSize;
   char *token;
 
+  char *command;
+  char comm[5][20];
+  for (int i = 0; i < 5; i++) {
+    strcpy(comm[i], "");}
+
   input = fopen(argv[1], "r");
 
   cBuffer = (char *)malloc(bufferSize * sizeof(char));
-
   if(cBuffer == NULL){
     printf("Error! Unable to allocate input buffer. \n");
 	  exit(1);}
+
+  int temp_index = -1;
 
   do {
     inputSize = getline(&cBuffer, &bufferSize, input);
@@ -29,13 +37,34 @@ int main(int argc, char *argv[]) {
     if (token == NULL){break;}
 
     while(token != NULL && strcmp(token, "\n")) {
-        printf("Token: %s\n", token);
+
+        if (temp_index == -1){
+          command = token;
+        }
+        else {
+          strcpy(comm[temp_index], token);
+        }
+        temp_index += 1;
         token = strtok(NULL, " \n");
     }
+
+    printf("%s %s %s %s %s %s\n", command, comm[0], comm[1], comm[2], comm[3], comm[4]);
+
+    for (int i = 0; i < 5; i++) {
+      strcpy(comm[i], "");}
+
+    temp_index = -1;
+
   } while(!feof(input));
 
   free(cBuffer);
   fclose(input);
+
+
+
+
+
+
   return 0;
 
 }//end of main()
