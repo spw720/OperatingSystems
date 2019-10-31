67be40aaafe0c742a965518c45a7a78050a2e8a5
Sean Wilson <swilso17@uoregon.edu>
2019-10-30 17:58:11 -0700

    commit via shell script

--- Project2/concept.c
+++ Project2/concept.c
@@ -20,6 +20,10 @@ int main() {
   char *args3[] = {"./iobound", "-minutes", "1", NULL};
   char *args4[] = {"./cpubound", "-minutes", "1", NULL};
 
+  char *args9[] = {"sleep", "5", NULL};
+  char *args8[] = {"sleep", "5", NULL};
+  char *args7[] = {"sleep", "5", NULL};
+
   for (int i = 0; i < 5; i++) {
 
     array[i] = fork();
@@ -28,7 +32,7 @@ int main() {
       printf("FORK ERROR\n");
     }
     if (array[i] == 0){
-
+      /*
       if(i == 0){
         printf("Fire: ls\n");
         execvp(args0[0], args0);
@@ -47,6 +51,7 @@ int main() {
         printf("ERROR: invalid\n");
         exit(-1);
       }
+
       else if(i == 3){
         printf("Fire: iobound\n");
         execvp(args3[0], args3);
@@ -57,6 +62,38 @@ int main() {
         printf("Fire: cpubound\n");
         execvp(args4[0], args4);
         printf("ERROR: cpubound\n");
+        exit(-1);
+      }
+      */
+
+      if(i == 1){
+        printf("Fire: sleep\n");
+        execvp(args9[0], args9);
+        printf("ERROR: sleep\n");
+        exit(-1);
+      }
+      else if(i == 2){
+        printf("Fire: sleep\n");
+        execvp(args8[0], args8);
+        printf("ERROR: sleep\n");
+        exit(-1);
+      }
+      else if(i == 3){
+        printf("Fire: sleep\n");
+        execvp(args7[0], args7);
+        printf("ERROR: sleep\n");
+        exit(-1);
+      }
+      else if(i == 4){
+        printf("Fire: sleep\n");
+        execvp(args7[0], args7);
+        printf("ERROR: sleep\n");
+        exit(-1);
+      }
+      else if(i == 5){
+        printf("Fire: sleep\n");
+        execvp(args7[0], args7);
+        printf("ERROR: sleep\n");
         exit(-1);
       }
 
