a1c45b8acc41c3d77c9ad235d52136c468f9404f
Sean Wilson <swilso17@uoregon.edu>
2019-10-31 21:05:33 -0700

    commit via shell script

--- Project2/lab5code.c
+++ Project2/lab5code.c
@@ -15,8 +15,8 @@
 
 void handler(int signal){
 
-  //printf("Child process: <%d> received signal: <%d>\n", getpid(), signal);
-  write(STDOUT_FILENO, "Child received signal!\n", strlen("Child received signal!\n"));
+  printf("Child process: <%d> received signal: <%d>\n", getpid(), signal);
+  //write(STDOUT_FILENO, "Child received signal!\n", strlen("Child received signal!\n"));
 }
 
 //---------------------------------------------------------------------------
