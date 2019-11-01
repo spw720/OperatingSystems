994be9769aae5b5f2f026ed9221a15db7a88ff4b
Sean Wilson <swilso17@uoregon.edu>
2019-10-31 17:20:50 -0700

    commit via shell script

--- Project2/lab5code.c
+++ Project2/lab5code.c
@@ -13,29 +13,12 @@
 
 void handler(int signal){
 
-  pid_t pid = getpid();
-  printf("Child process: <%i> received signal: <%d>\n", pid, signal);
-
-  if (signal == 0){
-    int sig;
-    sigset_t sigset;
-    int result = sigwait(&sigset, &sig);
-    if(result == 0){
-      printf("sigwait got signal: %d\n", sig);
-      kill(pid, SIGINT);
-    }
-  }
-
-  //if signal == 0: sigwait()
-  //else do sigaction? With passed in signal??
+  printf("Child process: <%d> received signal: <%d>\n", getpid(), signal);
 
 }
 
-void signaler(pid_t arr){
-  kill(arr, SIGUSR1);
-  kill(arr, SIGUSR1);
-  kill(arr, SIGINT);
-  /*
+void signaler(pid_t arr[]){
+
   for (int i = 0; i < 5; i++) {
     kill(arr[i], SIGUSR1);
   }
@@ -45,28 +28,25 @@ void signaler(pid_t arr){
   for (int j = 0; j < 5; j++) {
     kill(arr[j], SIGINT);
   }
-  */
+
 }//end of signaler
 
 
-int main(int argc, char *argv[]) {
+int main() {
 
-  signal(SIGUSR1, &handler);
+  signal(SIGUSR1, handler);
 
-  //pid_t pid[5];
-  pid_t pid;
+  pid_t pid[5];
+  //pid_t pid;
 
-  //for (size_t i = 0; i < 5; i++) {
+  for (size_t i = 0; i < 5; i++) {
 
-    pid = fork();
+    pid[i] = fork();
     //kill(pid[i], SIGUSR1);
 
-    if (pid < 0){perror("fork");exit(EXIT_FAILURE);}
+    if (pid[i] < 0){perror("fork");exit(EXIT_FAILURE);}
 
-    else if (pid == 0){
-
-      kill(pid, SIGUSR1);
-      kill(pid, SIGUSR1);
+    else if (pid[i] == 0){
 
       while(1) {
   			printf("	Child Process: %i - Running infinite loop...\n", getpid());
@@ -75,8 +55,10 @@ int main(int argc, char *argv[]) {
 
     }//end of if pid == 0
 
-  //}//end of for(range 5)
+  }//end of for(range 5)
 
+  sleep(5);
+  printf("Parent executing signaler\n");
   signaler(pid);
 
   int status;
