bfa65c0c681e58bf167f640fa2e5edd9ba951290
Sean Wilson <swilso17@uoregon.edu>
2019-11-07 21:28:46 -0800

    commit via shell script

--- Project2/part3.c
+++ Project2/part3.c
@@ -33,7 +33,7 @@ void alarm_handler(int signal){
   }
 
   int flag_boi = 5;
-  while (flag_boi > 0){
+  while (flag_boi >= 0){
 
     if(running_child + 1 >= pool_index ){
       running_child = 0;
