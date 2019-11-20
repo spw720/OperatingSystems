b7546b5808668be54ff23aae26aed9c6d33bf0d3
Sean Wilson <swilso17@uoregon.edu>
2019-11-19 19:57:03 -0800

    commit via shell script

--- Lab8/lab8.c
+++ Lab8/lab8.c
@@ -2,14 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 
+//-----------------------------------------------------------------------------
+
 #define MAXNAME 100
 #define MAXQUEUES 10
+
+#define BUFFER_SIZE 8
+
+//-----------------------------------------------------------------------------
 
 struct mealTicket {
   int ticketNum;
   char *dish;
 };
 typedef struct mealTicket mealTicket;
+
+//-----------------------------------------------------------------------------
 
 struct MTQ {
   char *name[MAXNAME];
@@ -20,11 +28,17 @@ struct MTQ {
 };
 typedef struct MTQ MTQ;
 
+//-----------------------------------------------------------------------------
+
 int enqueue(char *MTQ_ID, mealTicket *MT){
+
+
 
   return 0;
 
 }//end of enqueue()
+
+//-----------------------------------------------------------------------------
 
 int dequeue(char *MTQ_ID, int ticketNum, mealTicket *MT){
 
@@ -32,27 +46,39 @@ int dequeue(char *MTQ_ID, int ticketNum, mealTicket *MT){
 
 }//end of dequeue()
 
+//-----------------------------------------------------------------------------
 
 MTQ *registry[MAXQUEUES];
 
+//-----------------------------------------------------------------------------
 
 int main(){
 
   //initialize all MTQ structs
   MTQ breakfast;
   *breakfast.name = "breakfast";
+  mealTicket buffer1[BUFFER_SIZE];
+  breakfast.buffer = &buffer1;
+
   MTQ lunch;
   *lunch.name = "lunch";
+  mealTicket buffer2[BUFFER_SIZE];
+  lunch.buffer = &buffer2;
+
   MTQ dinner;
   *dinner.name = "dinner";
+  mealTicket buffer3[BUFFER_SIZE];
+  dinner.buffer = &buffer3;
+
   MTQ bar;
   *bar.name = "bar";
+  mealTicket buffer4[BUFFER_SIZE];
+  bar.buffer = &buffer4;
 
   printf("BFAST: %s\n", *breakfast.name);
   printf("LUNCH: %s\n", *lunch.name);
   printf("DINNER: %s\n", *dinner.name);
   printf("BAR: %s\n", *bar.name);
-
 
   //push all MTQ's onto register
   registry[0] = &breakfast;
@@ -93,3 +119,5 @@ int main(){
 
 
 }//end of main()
+
+//-----------------------------------------------------------------------------
