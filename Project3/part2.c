fb55bb0a62a94966f30020411fec567377d39ac4
Sean Wilson <swilso17@uoregon.edu>
2019-12-01 20:57:37 -0800

    commit via shell script

--- Project3/part2.c
+++ Project3/part2.c
@@ -567,10 +567,22 @@ int main(int argc, char const *argv[]) {
       pub_avail[i] = 0;
     }
   }
+  for (size_t i = 0; i < NUMPROXIES; i++) {
+    if(sub_avail[i] == 1){
+      pthread_cancel(sub_pool[i]);
+      //set thread to available
+      sub_avail[i] = 0;
+    }
+  }
 
   for (size_t i = 0; i < NUMPROXIES; i++) {
     if(pub_avail[i] == 1){
       pthread_join(pub_pool[i], NULL);
+    }
+  }
+  for (size_t i = 0; i < NUMPROXIES; i++) {
+    if(sub_avail[i] == 1){
+      pthread_join(sub_pool[i], NULL);
     }
   }
 
