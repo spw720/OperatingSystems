5afa4b564733ffb25d0c44d58d8cfcd2c63f762e
Sean Wilson <swilso17@uoregon.edu>
2019-12-02 18:48:42 -0800

    commit via shell script

--- Project3/part2.c
+++ Project3/part2.c
@@ -394,7 +394,7 @@ void *publisher(void *input){ //enqueue()
 void *subscriber(void *input){ //getEntry()
 
   sub_args *inp = input;
-  printf("*\tsubscriber(): IS THIS SHIT WORKIN??[%s]\n", input.tobe_sub[0]);
+  printf("*\tsubscriber(): IS THIS SHIT WORKIN??[%s]\n", inp->tobe_sub[0]);
   sleep(1);
 
   //empty struct to-be filled by getEntry()
