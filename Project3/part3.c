feeaaf0ee729abe0553c7f69f2373c09f4f069b0
Sean Wilson <swilso17@uoregon.edu>
2019-12-02 22:15:59 -0800

    commit via shell script

--- Project3/part3.c
+++ Project3/part3.c
@@ -427,7 +427,7 @@ int main(int argc, char const *argv[]) {
     int tokens = 0;
     int arguments = 0;
 
-    file_size = getline(&buffy, &bufferSize, input);
+    //file_size = getline(&buffy, &bufferSize, input);
 
     for (int i = 0; i < file_size; i++) {
       if (buffy[i] == ' '){spaces += 1;}
