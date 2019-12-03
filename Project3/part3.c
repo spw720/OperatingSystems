ce4f60425f9ae2a1f5b0d916d2d635d204671be7
Sean Wilson <swilso17@uoregon.edu>
2019-12-03 14:34:43 -0800

    commit via shell script

--- Project3/part3.c
+++ Project3/part3.c
@@ -470,43 +470,43 @@ int main(int argc, char const *argv[]) {
 
     //printf("ARGUMENTS: %s %s %s\n", args[0], args[1], args[2]);
 
-    for (size_t i = 0; i < tokens; i++) {
 
-      if (strcmp(args[0], "create")==0){
-        printf("FOUND CREATE OP\n");
-      }
-      //-----------------------------------
-      if (strcmp(args[0], "delta")==0){
-        printf("FOUND DELTA OP\n");
-      }
-      //-----------------------------------
-      if (strcmp(args[0], "add")==0){
-        if (strcmp(args[1], "publisher")==0){
-          printf("FOUND ADD PUB OP\n");
-        }
-        if (strcmp(args[1], "subscriber")==0){
-          printf("FOUND ADD SUB OP\n");
-        }
-      }
-      //-----------------------------------
-      if (strcmp(args[0], "query")==0){
-        if (strcmp(args[1], "topics")==0){
-          printf("FOUND QUERY TOPICS OP\n");
-        }
-        if (strcmp(args[1], "publishers")==0){
-          printf("FOUND QUERY PUB OP\n");
-        }
-        if (strcmp(args[1], "subscribers")==0){
-          printf("FOUND QUERY SUB OP\n");
-        }
-      }
-      //-----------------------------------
-      if (strcmp(args[0], "start")==0){
-        printf("FOUND START OP\n");
-      }
-      //-----------------------------------
 
+    if (strcmp(args[0], "create")==0){
+      printf("FOUND CREATE OP\n");
     }
+    //-----------------------------------
+    if (strcmp(args[0], "delta")==0){
+      printf("FOUND DELTA OP\n");
+    }
+    //-----------------------------------
+    if (strcmp(args[0], "add")==0){
+      if (strcmp(args[1], "publisher")==0){
+        printf("FOUND ADD PUB OP\n");
+      }
+      if (strcmp(args[1], "subscriber")==0){
+        printf("FOUND ADD SUB OP\n");
+      }
+    }
+    //-----------------------------------
+    if (strcmp(args[0], "query")==0){
+      if (strcmp(args[1], "topics")==0){
+        printf("FOUND QUERY TOPICS OP\n");
+      }
+      if (strcmp(args[1], "publishers")==0){
+        printf("FOUND QUERY PUB OP\n");
+      }
+      if (strcmp(args[1], "subscribers")==0){
+        printf("FOUND QUERY SUB OP\n");
+      }
+    }
+    //-----------------------------------
+    if (strcmp(args[0], "start")==0){
+      printf("FOUND START OP\n");
+    }
+    //-----------------------------------
+
+    
 
 
 
