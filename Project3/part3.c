ceb236d5f5042c59d2a3b88de69b49845c0b3376
Sean Wilson <swilso17@uoregon.edu>
2019-12-04 17:49:24 -0800

    commit via shell script

--- Project3/part3.c
+++ Project3/part3.c
@@ -633,6 +633,7 @@ int main(int argc, char const *argv[]) {
               }
               else{checkT ++;}
             }
+            printf("CHECKT:%d\n", checkT);
             if(checkT == MAXTOPICS-1){
               printf("There are no topics you fool!\n");
             }
